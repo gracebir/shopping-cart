99b16eb61e0bfd2ce87a4b39932b31a386bcc00e
graceDev1 <grace.devolop@gmail.com>
2021-07-27 19:49:29 +0200

    list all the items on the checkout page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,17 @@
 import { ShoppingCartIcon } from '@heroicons/react/outline';
 import { useSelector } from 'react-redux';
 import { selectItems } from '../slices/basketSlice';
-
+import { useRouter } from 'next/router';
 
 function Header() {
     const items = useSelector(selectItems);
+    const route = useRouter();
+
     return (
         <header className='sticky top-0 z-50'>
             <div className='flex justify-around md:justify-around items-center bg-shopping_blue p-1 flex-grow py-2'>
                 <div className='mt-2 flex flex-grow sm:flex-grow-0'>
-                    <h1 className='text-white text-2xl font-bold cursor-pointer'>ShopRITE.</h1>
+                    <h1 onClick={()=> route.push('/')} className='text-white text-2xl font-bold cursor-pointer'>ShopRITE.</h1>
                 </div>
                 <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
                     <div className='link'>
@@ -18,7 +20,7 @@ function Header() {
                     </div>
                     <div className='relative link flex items-center'>
                         <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 rounded-full text-black font-bold text-center'>{items.length}</span>
-                        <ShoppingCartIcon className='h-10' />
+                        <ShoppingCartIcon onClick={()=> route.push('/checkout')} className='h-10' />
                     </div>
                 </div>
             </div>
--- client/src/slices/basketSlice.ts
+++ client/src/slices/basketSlice.ts
@@ -12,7 +12,18 @@ export const BasketSlice = createSlice({
         addToBasket: (state:any, action:any) => {
             state.items = [...state.items, action.payload]
         },
-        removeFromBasket: (state, action) => {}
+        removeFromBasket: (state, action) => {
+            const index: number = state.items.findIndex(
+                (baskItem: any) => baskItem.id === action.payload.id
+            );
+            let newBasket = [...state.items];
+            if(index >= 0){
+                newBasket.splice(index,1);
+            } else{
+                console.warn(`id item does not exists ${action.payload.id}`)
+            }
+            state.items = newBasket;
+        }
     }
 });
 
@@ -26,3 +37,4 @@ export default BasketSlice.reducer;
 
 
 export const selectItems = (state:any) => state.basket.items;
+export const selectTotal = (state:any) => state.basket.items.reduce((total:any,b:any)=> total + b.price,0);
